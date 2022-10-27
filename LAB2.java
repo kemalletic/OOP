e677635a9733d70f423afc93cadcd6853b0f5d17
Kemal Letić <116316897+kemalletic@users.noreply.github.com>
2022-10-27 15:11:58 +0200

    Add files via upload

---
+++ LAB2.java
@@ -0,0 +1,98 @@
+package lab22;
+import java.util.Scanner;
+
+public class Lab2week {
+
+public static void main(String[] args) {
+//
+
+/*
+Scanner reader = new Scanner(System.in);
+String correct_password = "carrot";
+int correct = 0;
+while (correct != 1) {
+System.out.println("Enter password: ");
+String user_password = reader.nextLine();
+if(!correct_password.equals(anObject:user_password)) {
+System.out.println("Enter password: ");
+user_password = reader.nextLine();
+
+} else }
+  correct = 1;
+}
+
+System.out.println("Secret is hello!");
+}
+ int sum = 0;
+ 
+ int num;
+ for (int i = 0; i < 3; i++) {
+System.out.println("Type the " + (i+1)+ ". number: ");
+num = Integer.parseInt(reader.nextLine());
+sum += num;
+ }
+ System.out.println(sum);
+
+Scanner reader = new Scanner(System.in);
+
+ int sum = 0;
+ int num;
+ while(true){
+System.out.println("Type the number:");
+num = Integer.parseInt(reader.nextLine());
+
+if(num==0){
+break;
+}
+sum += num;
+System.out.println("Sum: "+sum);
+}
+ System.out.println("Sum at the end: "+ sum);
+ }
+}
+
+Scanner reader = new Scanner(System.in);
+System.out.println("Type the first number: ");
+int start = Integer.parseInt(reader.nextLine());
+
+System.out.println("Type the end number: ");
+int end = Integer.parseInt(reader.nextLine());
+
+while(start <= end) {
+System.out.println(start);
+start++;
+
+}
+
+
+Scanner reader = new Scanner(System.in);
+System.out.println("Type the power: ");
+int power = Integer.parseInt(reader.nextLine());
+int sum = 0;
+
+for(int i=0; i<=power; i++){
+sum = sum + (int)Math.pow(2,1);
+}
+
+int i = 0;
+while(i <= power){
+sum = sum + (int)Math.pow(2,1);
+i++;
+}
+
+printText();
+}
+public static void printText() {
+String mystr = "In the beginning there were the swamp, the hoe, and the java";
+System.out.println(mystr);
+}
+}
+
+public static void main(String[] args) {
+printStars(number3);
+}
+
+   public static void printStars(int number){
+    for(int 1=0; 3 < number; i++) {
+    System.out.print("");
+*/
